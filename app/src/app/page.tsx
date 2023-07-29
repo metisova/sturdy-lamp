e6b206f5ef7024ee90a6f08b41bc9fafe7d9ddb3
Daria Doronina <skidle.dd@gmail.com>
2023-07-29 20:28:22 +0200

    removed unnecessary changes

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -2,8 +2,6 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
-  
-
   return (
     <main className={styles.main}>
       <div className={styles.description}>
