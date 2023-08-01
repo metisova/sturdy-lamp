fab305966be5cb486184076ddfe8ee97330c00c6
Anna Kopylova <anna.metisova@gmail.com>
2023-08-01 19:04:18 +0200

    added dashboard route

---
+++ app/src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return  (
+    <div>
+      <h1>You are on a Dashboard page!</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, laudantium voluptatibus. Dolorem, dolore earum quis corrupti voluptatum delectus esse, harum aliquam ex quo adipisci culpa minima qui suscipit autem velit.</p>
+    </div>
+  );
+}
