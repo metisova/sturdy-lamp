49186724a2a538ef358feb6dfa2beaa9727413a6
Daria Doronina <skidle.dd@gmail.com>
2023-08-01 19:43:09 +0200

    add eslint config (#34)
    * add eslint config
    
    * cleanup

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,4 +1,8 @@
 import Link from 'next/link'
+
+const sum = (a: number, b: number) => a * b;
+
+export const result = sum('two', 'one');
  
 export default function Page() {
   return <Link href="/dashboard">Dashboard page</Link>
