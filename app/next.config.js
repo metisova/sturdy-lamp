9388609df813d9f5eda351b8a155a6b309bb03e1
Daria Doronina <skidle.dd@gmail.com>
2023-08-01 19:55:19 +0200

    test for deploy

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        outputFileTracingExcludes: {
+            "*": [
+                "node_modules/@swc/core-linux-x64-gnu",
+                "node_modules/@swc/core-linux-x64-musl",
+                "node_modules/@esbuild/linux-x64",
+            ],
+        },
+    }
+}
 
 module.exports = nextConfig
