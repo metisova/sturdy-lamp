bfe15ad3f85b0f9139e70fc31d162f3c4a294d8d
Daria Doronina <skidle.dd@gmail.com>
2023-08-06 11:19:23 +0200

    trigger redeploy

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div>
-      <h1>You are on a Dashboard page!</h1>
+      <h1>You are on a Dashboard page!!</h1>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas,
         laudantium voluptatibus. Dolorem, dolore earum quis corrupti voluptatum
