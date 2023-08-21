492b214ce7a43e9b0ba26c435cbf1d83f1ead42a
Daria Doronina <skidle.dd@gmail.com>
2023-08-21 11:44:20 +0200

    wip

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,16 @@
+import { Box, AppBar, Container } from '@mui/material';
+
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <section>
-      {/* Include shared UI here e.g. a header or sidebar */}
-      <nav />
-
-      {children}
-    </section>
+    <Box>
+      <AppBar>navbar</AppBar>
+      <Container>
+        {children}
+      </Container>
+    </Box>
   );
 }
