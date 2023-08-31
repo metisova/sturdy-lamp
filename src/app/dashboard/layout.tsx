15fc129250527719b7c5d5174b585c62768d7884
Daria Doronina <skidle.dd@gmail.com>
2023-08-31 14:35:50 +0200

    prettier

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 import { Box, AppBar, Toolbar, Typography } from "@mui/material";
-import Main from '../../components/Main';
+import Main from "../../components/Main";
 
 export default function DashboardLayout({
   children,
@@ -8,14 +8,18 @@ export default function DashboardLayout({
 }) {
   return (
     <Box>
-      <AppBar position="absolute" elevation={0} sx={{ backgroundColor: '#fff', color: '#000', borderBottom: '1px solid #e0e0e0' }}>
+      <AppBar
+        position="absolute"
+        elevation={0}
+        sx={{
+          backgroundColor: "#fff",
+          color: "#000",
+          borderBottom: "1px solid #e0e0e0",
+        }}
+      >
         <Toolbar>
           {/* TODO add logo */}
-          <Typography
-            component="h1"
-            variant="h6"
-            sx={{ flexGrow: 1 }}
-          >
+          <Typography component="h1" variant="h6" sx={{ flexGrow: 1 }}>
             Sturdy Lamp
           </Typography>
           {/* TODO add user avatar */}
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,18 +1,14 @@
 import { Box, Container } from "@mui/material";
 
-function Main({
-  children,
-}: {
-    children: React.ReactNode;
-  }) {
+function Main({ children }: { children: React.ReactNode }) {
   return (
     <Box
       component="main"
       sx={{
-        backgroundColor: '#f5f5f5',
+        backgroundColor: "#f5f5f5",
         flexGrow: 1,
-        height: '100vh',
-        overflow: 'auto',
+        height: "100vh",
+        overflow: "auto",
       }}
     >
       <Container>{children}</Container>
